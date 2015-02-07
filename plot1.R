ce31ebb34a3859bed6c6ae0fa464edd99ab9fa78
ShwetaGrewal <ShwetaGrewal@users.noreply.github.com>
2015-02-08 00:47:47 +0530

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,7 @@
+data<-read.table("D:\\CourseraCourses\\ExploratoryDataAnalysis\\household_power_consumption.txt", sep=";", header=T, stringsAsFactors=F)
+data$Date<-as.Date(data$Date, "%d/%m/%y%y")
+data<-subset(data,Date>=as.Date("2007-02-01") & Date<=as.Date("2007-02-02"))
+data$Global_active_power<-as.numeric(data$Global_active_power)
+png("D:\\CourseraCourses\\ExploratoryDataAnalysis\\plot1.png", width = 480, height = 480)
+hist(data$Global_active_power,main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="red")
+dev.off()
